f5e92937aadc804615cfbad8c2ecacf4d9c67d9c
isharadasuni <isharadasuni160@gmail.com>
2024-11-30 14:23:00 +0530

    start login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login/Login";
+
 function App() {
+  
   return (
-  <div>hi</div>
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<Login />} />
+
+</Routes>
+</BrowserRouter>
+    
   );
 }
 
