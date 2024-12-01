099c227301e19155b3d8021465d67a5b626b6ad0
isharadasuni <isharadasuni160@gmail.com>
2024-12-01 12:39:57 +0530

    complete design of login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,9 +1,43 @@
-import React  from 'react';
-
+import React from 'react';
+import "../../Pages/Login/login.scss";
 
 const Login = () => {
-    return(
-        <div>login</div>
+
+
+    return (
+
+        <div className="login">
+
+            <div className="login-container">
+
+                <div className="topic">
+                    Login
+                </div>
+
+                <div className="inputLogin">
+                   
+                        <input type="email" placeholder="Username" required />
+                 
+                        <input type="password" placeholder="Password" required />
+                  
+                </div>
+
+                <div className="forgetPassword">
+                    Forgot Password?
+                </div>
+
+                <div >
+                    <button>Login</button>
+                </div>
+
+                <div className="signUpLink">
+                    Don't have an Account? <b> &nbsp; Register</b>
+                </div>
+
+
+            </div>
+
+        </div>
     )
 }
 export default Login;
