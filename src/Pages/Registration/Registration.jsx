b6d2e59ffcae286aee5e976c5dbfada65eb1511d
isharadasuni <isharadasuni160@gmail.com>
2024-12-01 20:53:00 +0530

    start registration form

--- src/Pages/Registration/Registration.jsx
+++ src/Pages/Registration/Registration.jsx
@@ -5,10 +5,37 @@ const Registration = () => {
 
 
     return (
-        <div>
-            hi
-        </div>
 
+        <div className="registration">
+
+            <div className="regContainer">
+
+                <div className="regTopic">
+                    Registration Form
+
+                </div>
+
+                <div className="inputFeild">
+
+                    <input type="name" id="name" name="name" placeholder="Full Name" required />
+                    <input type="email" id="email" name="email" placeholder="Email" required />
+                    <input type="password" id="password" name="password" placeholder="Password" required />
+                    <input type="confirmPassword" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required />
+                </div>
+
+                <div>
+                    <button>Register</button>
+                </div>
+
+
+
+
+
+
+            </div>
+
+
+        </div>
     )
 }
 export default Registration;
